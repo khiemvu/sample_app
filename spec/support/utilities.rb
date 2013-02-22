e08d4afabb347052813a3fd7a57c0a4a26fd6f89
khiemvu <khiemvu.org@gmail.com>
2013-02-22 17:21:35 +0700

    Add user following

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -13,7 +13,11 @@ def valid_signin(user)
   fill_in "Password", with: user.password
   click_button "Sign in"
 end
-
+RSpec::Matchers::define :have_title do |text|
+  match do |page|
+    Capybara.string(page.body).has_selector?('title', text: text)
+  end
+end
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
